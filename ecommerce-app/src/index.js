60fbeaf10a8d841f2261f2414ed4244fe41b4c9e
Evgeny Kuptsov <kupzov200@mail.ru>
2024-05-04 16:00:13 +0400

    core 1_08: install and configure webpack

---
+++ ecommerce-app/src/index.js
@@ -0,0 +1,7 @@
+import './index.html';
+import './global.css';
+
+const mult = (a, b) => a * b;
+
+console.log(mult(3, 4));
+console.log(mult(3, 5));
---
+++ ecommerce-app/webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const postCssPresetEnv = require('postcss-preset-env');
+const path = require('path');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+module.exports = {
+  mode,
+  target,
+  devtool,
+  devServer: {
+    port: 3000,
+    open: true,
+  },
+  entry: path.resolve(__dirname, './src/index'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: 'index.[contenthash].js',
+    assetModuleFilename: 'assets/[name][ext]'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [postCssPresetEnv()]
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|webp|gif|svg)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  }
+}
