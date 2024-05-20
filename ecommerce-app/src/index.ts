35fe8da3200cb676b038060e859bc9cec3c4b313
kupzov2000 <47789807+kupzov2000@users.noreply.github.com>
2024-05-20 12:18:03 +0400

    feat/RSS-ECOMM-2_RegistrationPageInputValidation (#9)
    * feat 2_09-2_10: implement client-side validation for required fields in the registration form and display clear error messages for incorrect input in the registration form
    
    * merge: integrate 'release/RSS-ECOMM-2_LoginRegistrationMain' into 'feat/RSS-ECOMM-2_RegistrationPageInputValidation'
    
    * fix: fix .gitignore file

--- ecommerce-app/src/index.ts
+++ ecommerce-app/src/index.ts
@@ -2,3 +2,5 @@ import './global.css';
 import LogInPage from './app/pages/log-in-page/log-in';
 
 new LogInPage('log-in-page').renderPage();
+// import RegistrationPage from './app/pages/registration-page/registration';
+// new RegistrationPage().renderPage();
